d9268f563a26425fcef59f844054faa75b65c532
LucasFerreira92 <lucasferreirasurf13@gmail.com>
2024-09-24 16:35:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const searchInput = document.querySelector(".search-input");
 
-const API_KEY = "5534b8884fb24388939102358240908";
+const API_KEY = "2072570e7c7b4afa9e6192309242409";
 
 const getWeatherDetails = async (cityName) => {
-    const API_URL = 'http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}';
+    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`;
 
     try {
         //fetch weather data form the API and parse the response as JSON
         const response = await fetch(API_URL);
         const data = await response.json();
 
+        const temperature = 
         console.log(data)
     } catch (error) {
-
+        console.log(error);
     }
         
 }
