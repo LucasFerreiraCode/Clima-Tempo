6ba6ba3e6740def259bdd1c5431871513b1169e8
LucasFerreira92 <lucasferreirasurf13@gmail.com>
2024-09-24 15:49:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const searchInput = document.querySelector(".search-input");
 
-searchInput.addEventListener("keyup", () => {
+const getWeatherDetails = (cityName) => {
+    const API_URL = 'http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}';
+}
+
+searchInput.addEventListener("keyup", (e) => {
     const cityName = searchInput.value.trim();
 
-    if(e.key == "Enter" && cityName) {
-        console.log(cityName);
+    if (e.key == "Enter" && cityName) {
+        getWeatherDetails(cityName);
     }
 });
